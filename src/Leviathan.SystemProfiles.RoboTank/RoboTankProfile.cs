847a4c8b5e89605ed3887f33722e84b747f996f2
mik3c <mike.carleton@gmail.com>
2021-06-22 02:56:47 -0400

    Robo-Tank controller profile

--- src/Leviathan.SystemProfiles.RoboTank/RoboTankProfile.cs
+++ src/Leviathan.SystemProfiles.RoboTank/RoboTankProfile.cs
@@ -1,24 +1,15 @@
 ﻿using Leviathan.Alpha.Data.Npgsql;
-using Leviathan.Alpha.Database;
 using Leviathan.Components;
 using Leviathan.DbDataAccess;
-using Leviathan.DbDataAccess.Npgsql;
 using Leviathan.Hardware.I2C;
 using Leviathan.Hardware.PCA9685;
 using Leviathan.Hardware.RPIGPIO;
-using Leviathan.SDK;
 using Leviathan.SystemProfiles.Basic;
 using Leviathan.SystemProfiles.FactoryReset;
-using Leviathan.SystemProfiles.Hardcore;
-using Leviathan.SystemProfiles.Postgres;
 using Npgsql;
-using System;
-using System.Collections.Generic;
-using System.Device.Gpio;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
-using static Leviathan.Components.ComponentCategory;
+
 [assembly: LeviathanPlugin("Robo-Tank Profile")]
 namespace Leviathan.SystemProfiles.RoboTank {
 
@@ -30,7 +21,9 @@ namespace Leviathan.SystemProfiles.RoboTank {
 		ILeviathanAlphaDataContext<NpgsqlConnection> _context;
 		ILeviathanAlphaDataContextProvider Provider { get; }
 		ILeviathanAlphaDataContext<NpgsqlConnection> Context => _context ??= Provider.CreateContext<NpgsqlConnection>();
-		NpgsqlConnection Connection => Context.Connection;
+
+		static readonly int[] GPIOPins = new[] { 9, 10, 22, 15, 14, 23, 24, 25, 16, 6, 5, 7, 11, 8, 12, 13, 17, 27, 19, 20, 26, 21 };
+		static readonly int[] PWMPins = new[] { 15, 14, 13, 12, 11, 10, 9, 8, 0, 1, 2, 3, 4, 5, 6, 7 };
 
 		public RoboTankProfile(IComponentsService components, ILeviathanAlphaDataContextProvider contextProvider) :
 			base(components) {
@@ -39,6 +32,7 @@ namespace Leviathan.SystemProfiles.RoboTank {
 
 		public override async Task Apply() {
 			await base.ApplyRequired();
+			long c1 = 0, c2 = 0;
 
 			var drivers = new {
 				RESET_PROFILE = Register<FactoryResetProfile>(),
@@ -61,70 +55,32 @@ namespace Leviathan.SystemProfiles.RoboTank {
 				PWM = CreateModule("PCA9685", "Robo-Tank PWM", drivers.PWM)
 			};
 
-			long c1 = 0, c2 = 0;
-			var gpinout = new[] { 9, 10, 22, 15, 14, 23, 24, 25, 16, 6, 5, 7, 11, 8, 12, 13 }
-				.Select(p => new HardwareConnectorRecord {
-					Name = $"GPIO{++c1}",
-					Description = $"GPIO Pin {c1}",
-					ModuleId = modules.GPIO,
-					ComponentTypeId = drivers.GPIO_C,
-					ConnectorData = new GpioConnectorData {
-						Mode = 1,
-						Pin = p
-					}
-				}
-			);
-
-			var gpout = new[] { 17, 27, 19, 20, 26, 21 }.Select(p =>
-				new HardwareConnectorRecord {
-					Name = $"GPIO{c1++}",
-					Description = $"GPIO Pin {c1}",
-					ModuleId = modules.GPIO,
-					ComponentTypeId = drivers.GPIO_C,
-					ConnectorData = new GpioConnectorData {
-						Mode = 0,
-						Pin = p
-					}
-				}
-			);
-
-			var pwm = new[] { 15, 14, 13, 12, 11, 10, 9, 8, 0, 1, 2, 3, 4, 5, 6, 7 }.Select(p =>
-				new HardwareConnectorRecord {
-					Name = $"PWM{++c2}",
-					Description = $"PWM I/O {c2}",
-					ModuleId = modules.PWM,
-					ComponentTypeId = drivers.PWM_C,
-					ConnectorData = new PwmIOConnectorData {
-						PwmChannelId = p
-					}
-				}
-			);
-
-			var connectorIds = gpinout.Concat(gpout).Concat(pwm)
-				.Select(Context.HardwareConnector.Create)
-				.ToArray();
+			var connectors = new {
+				GPInOut = GPIOPins.Select(p => CreateConnector($"GPIO{++c1}", $"GPIO Pin {c1}", drivers.GPIO_C, modules.GPIO, new { Pin = p })).ToArray(),
+				PWM = PWMPins.Select(p => CreateConnector($"PWM{++c2}", $"PWM I/O {c2}", drivers.PWM_C, modules.PWM, new { PwmChannelId = p })).ToArray()
+			};
 
 			int c3 = 0, c4 = 0, c5 = 0, c6 = 0;
 			var channels = new {
-				AC_PORT = connectorIds[0..16].Select(i =>
+				AC_PORT = connectors.GPInOut[0..16].Select(i =>
 					CreateChannel(
 						$"AC{++c3:00}",
 						$"AC Outlet {c3:00}",
 						drivers.GPIO_OO_CH, i, new { Mode = 1 })
 					).ToArray(),
-				SR_PORT = connectorIds[16..22].Select(i =>
+				SR_PORT = connectors.GPInOut[16..22].Select(i =>
 					CreateChannel(
 						$"SS{++c4:00}",
 						$"Sensor Port {c4:00}",
 						drivers.GPIO_S_CH, i, new { Mode = 0 })
 					).ToArray(),
-				DC_PORT = connectorIds[22..30].Select(i =>
+				DC_PORT = connectors.PWM[0..8].Select(i =>
 					CreateChannel(
 						$"DC{++c5:00}",
 						$"DC Port {c5:00}",
 						drivers.PWM_CH, i)
 					).ToArray(),
-				PWM_PORT = connectorIds[30..^0].Select(i =>
+				PWM_PORT = connectors.PWM[8..^0].Select(i =>
 					CreateChannel(
 						$"PWM{++c6:00}",
 						$"PWM/Analog Port {c6:00}",
@@ -140,6 +96,15 @@ namespace Leviathan.SystemProfiles.RoboTank {
 			Name = name,
 			ComponentTypeId = typeId,
 			Description = description
+		});
+
+		long CreateConnector(string name, string description, long typeId, long moduleId, object connectorData = default) => Context.HardwareConnector.Create(new HardwareConnectorRecord {
+			Name = name,
+			ComponentTypeId = typeId,
+			Description = description,
+			ModuleId = moduleId,
+			ConnectorData = connectorData ?? new object()
+
 		});
 
 		long CreateChannel(string name, string description, long typeId, long connectorId, object channelData = default) => Context.HardwareChannel.Create(new HardwareChannelRecord {
